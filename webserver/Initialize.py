c7ca264e273131414e84c80b9c1e127993099c8e
Max Rodriguez <maxrcoppola@gmail.com>
2022-02-27 14:19:33 -0700

    flask: Render html template

--- webserver/Initialize.py
+++ webserver/Initialize.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-webapp = Flask(__name__)
+webapp = Flask(__name__, template_folder="www")
 
 @webapp.route("/")
 def root():
-    return render_template("www/login.html")
+    return render_template("login.html", var="test var")
 
 if __name__ == "__main__":
     webapp.run(host="0.0.0.0", port=80)
