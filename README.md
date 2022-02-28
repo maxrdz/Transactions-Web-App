394bdc0f0312fabcb5fc49f8895c4633619f4dd5
Max Rodriguez <maxrcoppola@gmail.com>
2022-02-28 14:02:43 -0700

    flask: Create systemd service for flask

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Transactions Web App Source
 ### To create the Python virtual environment, run `./linux/create-venv.sh`.
-### To launch the web application, run `./linux/start-server.sh`.
+### To start the systemd service unit, run `sudo ./linux/setup-service.sh`.
+### To manually start the server as a user, run `./linux/launch-server.sh`.
---
+++ linux/setup-service.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Creates the systemd service for Flask.
+
+# (Run from project root directory)
+# Usage: sudo ./linux/create-service.sh
+
+WORKING_DIR=$(pwd)
+PYTHON_VENV=$WORKING_DIR/.venv/bin/python3.10
+SYSTEMD=/etc/systemd/system
+SERVICE=flask.service
+
+cd $SYSTEMD
+touch $SERVICE
+echo "[Unit]
+Description=Flask Python Web Server
+After=network.target
+
+[Service]
+User=root
+WorkingDirectory=$WORKING_DIR
+ExecStart=$PYTHON_VENV -m webserver.Initialize
+Restart=always
+
+[Install]
+WantedBy=multi-user.target" > $SERVICE
+
+# Enable the Flask systemd service
+systemctl unmask flask.service
+systemctl enable flask.service
+systemctl start flask.service
