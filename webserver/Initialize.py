66f3c44301e6e9167550e3ffc3b88c2e26b31a83
Max Rodriguez <maxrcoppola@gmail.com>
2022-02-28 00:35:52 -0700

    flask: Serve image static content

--- webserver/Initialize.py
+++ webserver/Initialize.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, send_from_directory
 
-webapp = Flask(__name__, template_folder="www")
+_ROOT_DIR = "www"
+
+webapp = Flask(__name__, template_folder=_ROOT_DIR)
 
 @webapp.route("/")
 def root():
-    return render_template("login.html", var="test var")
+    return render_template("login.html")
+
+@webapp.route("/resources/<path:path>")
+def resources(path):
+    return send_from_directory("www/resources", path)
 
 if __name__ == "__main__":
     webapp.run(host="0.0.0.0", port=80)
