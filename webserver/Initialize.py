d99aaaed264b622594e1db2019f096838b373059
Max Rodriguez <maxrcoppola@gmail.com>
2022-02-28 18:35:00 -0700

    server: Support multiple languages

--- webserver/Initialize.py
+++ webserver/Initialize.py
@@ -1,16 +1,41 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory
+from flask import redirect, url_for
+from datetime import datetime
+from webserver.Localizer import Localizer
 
 _ROOT_DIR = "www"
-
 webapp = Flask(__name__, template_folder=_ROOT_DIR)
+Language = Localizer()  # Init the localizer class
 
-@webapp.route("/")
-def root():
-    return render_template("login.html")
+
+def localize_html(lang):
+    return render_template(
+        "login.html", v1=Language.LANG[lang]["PAGE_TITLE"],
+        v2=Language.LANG[lang]["LOGIN_HEADER"], v3=Language.LANG[lang]["USERNAME"],
+        v4=Language.LANG[lang]["PASSWORD"], v5=Language.LANG[lang]["LOGIN"],
+        v6=Language.LANG[lang]["SELECT_LANG"], v7=Language.LANG[lang]["SPANISH"],
+        v8=Language.LANG[lang]["ENGLISH"], year=datetime.now().year)
+
 
 @webapp.route("/resources/<path:path>")
 def resources(path):
     return send_from_directory("www/resources", path)
+
+
+@webapp.route("/es")
+def spanish():
+    return localize_html("SPANISH")
+
+
+@webapp.route("/en")
+def english():
+    return localize_html("ENGLISH")
+
+
+@webapp.route("/")
+def root():
+    return redirect(url_for(Language.DEFAULT))
+
 
 if __name__ == "__main__":
     webapp.run(host="0.0.0.0", port=80)
---
+++ webserver/Localizer.py
@@ -0,0 +1,28 @@
+# Languages Support
+
+
+class Localizer:
+    def __init__(self):
+        self.DEFAULT = "spanish"
+        self.LANG = {
+            "ENGLISH": {
+                "PAGE_TITLE": "Login to your account",
+                "LOGIN_HEADER": "Begin Session",
+                "USERNAME": "Enter username",
+                "PASSWORD": "Enter password",
+                "LOGIN": "Login to session",
+                "SELECT_LANG": "Select a language",
+                "SPANISH": "Spanish",
+                "ENGLISH": "English"
+            },
+            "SPANISH": {
+                "PAGE_TITLE": "Inicio de sesión",
+                "LOGIN_HEADER": "Iniciar Sesión",
+                "USERNAME": "Nombre de usuario",
+                "PASSWORD": "Contraseña",
+                "LOGIN": "Iniciar Sesión",
+                "SELECT_LANG": "Selecciona un idioma",
+                "SPANISH": "Español",
+                "ENGLISH": "Inglés"
+            }
+        }
