80d5787242ad43ddcaa439e77b3eafc6ee7e7d74
Max Rodriguez <maxrcoppola@gmail.com>
2022-02-28 22:58:02 -0700

    server: Cleaner html localizer

--- webserver/Initialize.py
+++ webserver/Initialize.py
@@ -1,20 +1,10 @@
 from flask import Flask, render_template, send_from_directory
 from flask import redirect, url_for
-from datetime import datetime
 from webserver.Localizer import Localizer
 
 _ROOT_DIR = "www"
 webapp = Flask(__name__, template_folder=_ROOT_DIR)
 Language = Localizer()  # Init the localizer class
-
-
-def localize_html(lang):
-    return render_template(
-        "login.html", v1=Language.LANG[lang]["PAGE_TITLE"],
-        v2=Language.LANG[lang]["LOGIN_HEADER"], v3=Language.LANG[lang]["USERNAME"],
-        v4=Language.LANG[lang]["PASSWORD"], v5=Language.LANG[lang]["LOGIN"],
-        v6=Language.LANG[lang]["SELECT_LANG"], v7=Language.LANG[lang]["SPANISH"],
-        v8=Language.LANG[lang]["ENGLISH"], year=datetime.now().year)
 
 
 @webapp.route("/resources/<path:path>")
@@ -23,13 +13,13 @@ def resources(path):
 
 
 @webapp.route("/es")
-def spanish():
-    return localize_html("SPANISH")
+def es():
+    return Localizer.localize_html(Language, "login.html", "es")
 
 
 @webapp.route("/en")
-def english():
-    return localize_html("ENGLISH")
+def en():
+    return Localizer.localize_html(Language, "login.html", "en")
 
 
 @webapp.route("/")
--- webserver/Localizer.py
+++ webserver/Localizer.py
@@ -1,21 +1,18 @@
+from flask import render_template
+from datetime import datetime
+
 # Languages Support
+# "/es" - Spanish
+# "/en" - English
 
 
 class Localizer:
+
     def __init__(self):
         self.DEFAULT = "spanish"
         self.LANG = {
-            "ENGLISH": {
-                "PAGE_TITLE": "Login to your account",
-                "LOGIN_HEADER": "Begin Session",
-                "USERNAME": "Enter username",
-                "PASSWORD": "Enter password",
-                "LOGIN": "Login to session",
-                "SELECT_LANG": "Select a language",
-                "SPANISH": "Spanish",
-                "ENGLISH": "English"
-            },
-            "SPANISH": {
+            "es": {
+                "HTML_LANG": "es",
                 "PAGE_TITLE": "Inicio de sesión",
                 "LOGIN_HEADER": "Iniciar Sesión",
                 "USERNAME": "Nombre de usuario",
@@ -24,5 +21,20 @@ class Localizer:
                 "SELECT_LANG": "Selecciona un idioma",
                 "SPANISH": "Español",
                 "ENGLISH": "Inglés"
+            },
+            "en": {
+                "HTML_LANG": "en",
+                "PAGE_TITLE": "Login to your account",
+                "LOGIN_HEADER": "Begin Session",
+                "USERNAME": "Enter username",
+                "PASSWORD": "Enter password",
+                "LOGIN": "Login to session",
+                "SELECT_LANG": "Select a language",
+                "SPANISH": "Spanish",
+                "ENGLISH": "English"
             }
         }
+
+    def localize_html(self, html, language):
+        return render_template(
+            html, Localizer=self, lang=language, date=datetime.now())
