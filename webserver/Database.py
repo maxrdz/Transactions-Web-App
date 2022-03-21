dd61b8ee7a2dc3cef8714bcaf4bd28fdb8e927fb
Max Rodriguez <maxrcoppola@gmail.com>
2022-03-21 14:25:34 -0700

    database: User Authentication

--- webserver/Database.py
+++ webserver/Database.py
@@ -1,8 +1,30 @@
-import pymysql
+import os
+from pymysql import connections
 from sqlalchemy import engine, create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from google.cloud.sql.connector import connector
+from sqlalchemy import Column, String, SMALLINT, TIMESTAMP, Integer
+
+DeclarativeBase = declarative_base()
+
+
+class User(DeclarativeBase):
+    __tablename__ = "users"
+    user_name = Column(String(10), primary_key=True)
+    password = Column(String(20))
+    first_name = Column(String(30))
+    last_name = Column(String(30))
+    status = Column(SMALLINT)
+    created_at = Column(TIMESTAMP)
+
+
+class Transaction(DeclarativeBase):
+    __tablename__ = "transactions"
+    entry_id = Column(Integer, primary_key=True)
+    timestamp = Column(TIMESTAMP)
+    topic = Column(String(20))
+    type = Column(SMALLINT)  # 0 = Spent, 1 = Deposit
+    amount = Column(Integer)
 
 
 class DatabaseManager:
@@ -12,34 +34,74 @@ class DatabaseManager:
     for user authentication and accessing transactions.
     """
     def __init__(self):
-        self.Base = declarative_base()
         self.configuration = {
+            "creds": "gcp-service-key.json",
             "instance": "wide-plating-343222:us-west4:transaction-db",
-            "mysql-driver": "pymysql",
-            "user": "server",
-            "password": "mysqldb554982",
-            "database": "TransactionDB"
+            "sql-driver": "pymysql",
+            "sql-user": "server",
+            "sql-pass": "mysqldbpass",
+            "sql-db": "TransactionDB"
         }
+        # GCP API account credentials
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = self.configuration['creds']
+
+        self.notify("Connecting to GCP Cloud MySQL server ..")
         self.engine = self.init_connection_engine()
-        self.session = sessionmaker(bind=self.engine)
+        session = sessionmaker(bind=self.engine)
+        self.session = session()
+        self.notify("MySQL session created successfully.")
+
+    def notify(self, string):
+        print(f"[{self.__class__.__name__}]: {string}")
 
     def init_connection_engine(self) -> engine.Engine:
-        def get_session() -> pymysql.connections.Connection:
-            session: pymysql.connections.Connection = connector.connect(
+        def get_connect() -> connections.Connection:
+            connect: connections.Connection = connector.connect(
                 self.configuration["instance"],
-                self.configuration["mysql-driver"],
-                user=self.configuration["user"],
-                password=self.configuration["password"],
-                db=self.configuration["database"],
+                self.configuration["sql-driver"],
+                user=self.configuration["sql-user"],
+                password=self.configuration["sql-pass"],
+                db=self.configuration["sql-db"],
             )
-            return session
+            return connect
 
         new_engine = create_engine(
-            f"mysql+{self.configuration['mysql-driver']}://",
-            creator=get_session,
+            f"mysql+{self.configuration['sql-driver']}://",
+            creator=get_connect,
         )
         return new_engine
 
-    def authenticate_user(self, user, password):
-        """Authenticate the username and password given."""
-        pass
+    def authenticate_user(self, username: str, password: str):
+        """
+        Query and authenticate user credentials given.
+        Returns query status, [user_found: ``Bool``, pass_matched: ``Bool``]
+        """
+        user_query = self.session.query(User)
+        query_status = [False, False]
+
+        for user in user_query:
+            if user.user_name == username:
+                query_status[0] = True
+        # If username not found, exit method.
+        if not query_status[0]:
+            return query_status
+
+        for user in user_query:
+            if user.user_name == username and user.password == password:
+                query_status[1] = True
+                return query_status
+        return query_status
+
+    def create_transaction(self, topic: str, t_type: int, amount: int):
+        """
+        Add a new transaction entry to the database. For
+        transaction type, 0 = Spent and 1 = Deposit.
+        """
+        transaction = Transaction()
+        transaction.entry_id = None  # TODO: Generate ID
+        transaction.timestamp = None  # TODO: Get Timestamp
+        transaction.topic = topic
+        transaction.type = t_type
+        transaction.amount = amount
+        self.session.add(transaction)
+        self.session.commit()  # Insert new User to db table.
--- webserver/Initialize.py
+++ webserver/Initialize.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, redirect, url_for
+from flask import Flask, send_from_directory, redirect, url_for, request
 from webserver.Localizer import SpanishLocalizer, EnglishLocalizer
 from webserver.Database import DatabaseManager
 
@@ -20,16 +20,29 @@ class FlaskWebApp:
         self.DEFAULT_LANG = self.SpanishLocalizer.HTML_LANG
         self.Database = DatabaseManager()
 
+        # User Authentication Query String
+        @self.Flask.route("/auth/", methods=["GET"])
+        def authentication_query():
+            query_strings = request.args
+            username = query_strings['username']
+            password = query_strings['password']
+
+            auth = self.Database.authenticate_user(username, password)
+            return f"Authentication Status Received: {auth}"
+
+        # HTTP Code 404 Page
         @self.Flask.errorhandler(404)
         def http_not_found(err_msg):
             """Return HTTP 404 not found page."""
             return self.EnglishLocalizer.localize_html(f"{self.HTTP_ERR}404.html")
 
+        # Serve Static Files
         @self.Flask.route("/<path:path>", methods=["GET"])
         def static_files(path):
             """Send static content such as css and images."""
             return send_from_directory(self.ROOT_DIR, path)
 
+        # Spanish Localizer
         @self.Flask.route("/es/<path:path>", methods=["GET"])
         def es(path):
             """Return site translated in Spanish."""
@@ -37,6 +50,7 @@ class FlaskWebApp:
                 return self.SpanishLocalizer.localize_html(path)
             return redirect(url_for("static_files", path=path))
 
+        # English Localizer
         @self.Flask.route("/en/<path:path>", methods=["GET"])
         def en(path):
             """Return site translated in English."""
@@ -44,10 +58,14 @@ class FlaskWebApp:
                 return self.EnglishLocalizer.localize_html(path)
             return redirect(url_for("static_files", path=path))
 
+        # Website Root
         @self.Flask.route("/", methods=["GET"])
         def root():
             """Website root, redirect to default language."""
             return redirect(url_for("es", path=self.INDEX_PAGE))
+
+    def notify(self, string):
+        print(f"[{self.__class__.__name__}]: {string}")
 
     def launch(self):
         """Starts the Flask web server with host/port."""
